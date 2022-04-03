857b190ca18743debeb9ef5f7241e0767e616ed2
Dnyaneshwar <dnyaneshwar.ware@walchandsangli.ac.in>
2022-04-03 14:46:02 +0530

    Added login screen and images

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
 import "react-native-gesture-handler";
 import Drawer from "./app/navigation/Drawer";
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
   return (
     <>
-      <Drawer />
+      <LoginScreen />
     </>
   );
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,31 +1,105 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import React from "react";
-import Carousel from "react-native-snap-carousel";
+import { statusbar } from "../configs/variables";
+import RNEInput from "../components/RNEInput";
+import AppButton from "../components/AppButton";
 
 const data = [
   {
     id: 1,
-    image: require("../illustrations/hiring.svg"),
+    image: require("../images/hiring.png"),
   },
   {
     id: 2,
-    image: require("../illustrations/step-up.svg"),
+    image: require("../images/step-up.png"),
+  },
+  {
+    id: 2,
+    image: require("../images/meeting.png"),
   },
 ];
 
 export default function LoginScreen() {
-  const cardItem = ({ index, item }) => {
+  const CardItem = ({ image }) => {
     return (
-      <View>
-        <Image source={item.image} />
+      <View
+        style={{
+          borderRadius: 5,
+          borderColor: "black",
+          borderWidth: 1,
+          marginHorizontal: 10,
+        }}
+      >
+        <Image
+          source={image}
+          style={{
+            width: 300,
+            height: 200,
+            resizeMode: "center",
+          }}
+        />
       </View>
     );
   };
   return (
-    <View>
-      <Carousel ref={(c) => {}} data={data} renderItem={cardItem} />
+    <View style={styles.container}>
+      <View style={styles.imagecontainer}>
+        <ScrollView horizontal>
+          {data.map((item, index) => (
+            <CardItem key={index} image={item.image} />
+          ))}
+        </ScrollView>
+      </View>
+      <View
+        style={{
+          marginHorizontal: 20,
+          marginVertical: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontWeight: "700",
+            fontSize: 20,
+          }}
+        >
+          Let's Get Started
+        </Text>
+      </View>
+      <View style={styles.userdetailscontainer}>
+        <View>
+          <RNEInput placeholder="E-mail" onInputChange={() => {}} bg="white" />
+          <RNEInput
+            placeholder="Password"
+            onInputChange={() => {}}
+            bg="white"
+          />
+        </View>
+
+        <View style={{ paddingHorizontal: 10 }}>
+          <AppButton
+            title="Sign In"
+            onPress={() => {}}
+            buttonStyles={{
+              paddingHorizontal: 10,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: statusbar,
+  },
+  imagecontainer: {
+    margin: 10,
+    flexDirection: "row",
+  },
+  userdetailscontainer: {
+    marginVertical: 10,
+    paddingHorizontal: 20,
+  },
+});
